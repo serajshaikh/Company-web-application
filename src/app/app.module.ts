e4d6d34f8ccb3fb6be4cddfee9cc11a8883dc2b1
serajshaikh <serazshaikh007@gmail.com>
2022-10-16 11:00:55 +0530

    I have created my first company web application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import {HttpClientModule} from '@angular/common/http'
+import { RouterModule,Routes } from '@angular/router';
+
+const approut: Routes = [
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'services',
+    component:ServicesComponent
+  },
+  {
+    path:"contact",
+    component:ContactComponent
+  },
+  {
+    path:"",
+    component:HomeComponent
+  }
+ 
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(approut)
   ],
   providers: [],
   bootstrap: [AppComponent]
